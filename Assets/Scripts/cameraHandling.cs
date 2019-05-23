579bca27560237af29571b0bede66d9e6bd804da
giuliom95 <giuliomartella1995@gmail.com>
2019-05-23 21:14:46 +0100

    Initial commit

---
+++ Assets/Scripts/cameraHandling.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cameraHandling : MonoBehaviour
+{
+    public Text coordText;
+    public GameObject sphere;
+    public GameObject cylinder;
+
+    List<Vector3> points = new List<Vector3>();
+
+    void Start()
+    {
+        points.Add(new Vector3());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        coordText.text = transform.position + "\n" + transform.rotation.eulerAngles;
+
+        Vector3 p = transform.localToWorldMatrix.MultiplyPoint(new Vector3(0, 0, .1f));
+        AddPoint(p);
+    }
+
+    void AddPoint(Vector3 p)
+    {
+        Vector3 v0 = points[points.Count - 1];
+        Vector3 v1 = p;
+        Vector3 d = v1 - v0;
+        float l = d.magnitude;
+        Quaternion r = Quaternion.LookRotation(d);
+        GameObject c = Instantiate(cylinder, v0, r);
+        Vector3 scale = c.transform.localScale;
+        scale.z = l;
+        c.transform.localScale = scale;
+        Instantiate(sphere, v1, Quaternion.identity);
+        points.Add(p);
+    }
+}
---
+++ Assets/Scripts/stroke.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stroke : MonoBehaviour
+{
+    public GameObject sphere;
+    public GameObject cylinder;
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<Vector3> points = new List<Vector3>();
+        Vector3 pnt = Random.insideUnitSphere;
+        Vector3 dir = Random.onUnitSphere;
+        points.Add(pnt);
+        for (int i = 0; i < 4000; ++i)
+        {
+            dir += 0.7f * Random.insideUnitSphere;
+            dir.Normalize();
+            pnt += 0.1f * dir;
+            points.Add(pnt);
+        }
+        
+
+        Instantiate(sphere, points[0], Quaternion.identity);
+        for(int i = 0; i < (points.Count - 1); ++i)
+        {
+            Vector3 v0 = points[i];
+            Vector3 v1 = points[i+1];
+            Vector3 d = v1 - v0;
+            float l = d.magnitude;
+            Quaternion r = Quaternion.LookRotation(d);
+            GameObject c = Instantiate(cylinder, v0, r);
+            Vector3 scale = c.transform.localScale;
+            scale.z = l;
+            c.transform.localScale = scale;
+            Instantiate(sphere, v1, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
